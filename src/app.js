ab1a9617f3e8b018a57952fa5fd9a0f6a801f3fe
weloin-soumyadip <soumyadip.dey@weloin.com>
2026-05-12 11:13:23 +0530

    logger added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
+import logger from '#config/logger.js';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from "cors";
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => logger.info(message.trim()),
+    },
+  })
+);
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
+  logger.info('Hello fro Acquisition!'); // using winston logger package
+
   res.status(200).send('Hello from Acquisition!');
 });
 
